7ff3cec6a8080b31b32c83abd3e2b599698a9e40
Y5ねこ <41314905+Y5neKO@users.noreply.github.com>
2024-09-25 23:49:18 +0800

    2024/09/25 Updated.
    23:49

---
+++ AKP.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "GUI.bat", 0, False
+WshShell.Run "SpringBoot.bat", 0, False
---
+++ SpringBoot.bat
@@ -0,0 +1,5 @@
+@SET JAVA_HOME=%~dp0\env\jdk8u411
+@SET Path=%JAVA_HOME%\bin;
+@echo %JAVA_HOME%
+@java -version
+@java -jar ArknightsPriestessProject.jar
