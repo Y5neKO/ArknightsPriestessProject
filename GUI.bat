95b0b6ab7f97cc730abb8089af61cb18d893dcaf
Y5ねこ <41314905+Y5neKO@users.noreply.github.com>
2024-09-27 00:59:09 +0800

    2024/09/27 Updated.
    切换自带环境为jre8u411，降低项目体积。

--- GUI.bat
+++ GUI.bat
@@ -1,37 +1,4 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGDR+kcbBydkXguIOWiuOp8d/unE3/iIqEgeQN4eaoTV07OGNPMj+EzhZoIoxWlqisQGCRhMQh2+ejM1u3pLsGWGI9Sgkh3qREGO7l8jK3d9gnDEiTgoUMFnkMYAxx+0/V/Dq4ADxTb6Rqxu
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
-@SET JAVA_HOME=%~dp0\env\jdk8u411
+@SET JAVA_HOME=%~dp0\env\jre8u411
 @SET Path=%JAVA_HOME%\bin;
 @echo %JAVA_HOME%
 @java -version
--- SpringBoot.bat
+++ SpringBoot.bat
@@ -1,4 +1,4 @@
-@SET JAVA_HOME=%~dp0\env\jdk8u411
+@SET JAVA_HOME=%~dp0\env\jre8u411
 @SET Path=%JAVA_HOME%\bin;
 @echo %JAVA_HOME%
 @java -version
